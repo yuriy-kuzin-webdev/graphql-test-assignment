d5cb7155230c335e0e23773c200ffce335bb0179
yuriy-kuzin-webdev <yuriy.kuzin.webdev@gmail.com>
2024-09-15 17:26:29 +0200

    feat: seeds added

---
+++ scripts/seed.js
@@ -0,0 +1,27 @@
+db = db.getSiblingDB('graphql');
+
+const date = new Date();
+
+const categories = Array.from({ length: 20 }).map((_, index) => ({
+  name: `Category ${index}`,
+  description: `Description for category ${index}`,
+  createdBy: 'Seed',
+  createdAt: date,
+  updatedAt: date,
+}));
+
+db.categories.insertMany(categories)
+
+categories.forEach((category) => {
+  const categoryId = db.categories.findOne({ name: category.name})._id;
+  const risks = Array.from({ length: 50 }).map((_, index) => ({
+    name: `Risk ${index}`,
+    description: `Description for risk ${index}`,
+    categoryId,
+    resolved: false,
+    createdBy: 'Seed',
+    createdAt: date,
+    updatedAt: date,
+  }))
+  db.risks.insertMany(risks);
+});
